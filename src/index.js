cfc347406f1282143237a0b0bbaf7faba41a508c
Nadin Severin <nadinlikes2018@gmail.com>
2022-12-09 21:26:17 +0200

    add project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './js/product-modal.js';
+import './js/mobile-menu';
+import './js/slider';
+import './js/slider-review';
+import './js/header-modal';
+import './js/about-modal';
+import './js/contacts-modal-1';
+import './js/contacts-modal-2';
+import './js/up-button.js';
